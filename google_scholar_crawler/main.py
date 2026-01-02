3d5ff1081f6053949900dc0b8c7b32918520af3f
Keriyar <202218140318@mail.sdufe.edu.cn>
2026-01-03 00:23:49 +0800

    fix: update google scholar id

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id("SSaBaioAAAAJ")
+author: dict = scholarly.search_author_id("bj7eQ6sAAAAJ")
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
